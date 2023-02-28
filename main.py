8923f65ad396de302b4e21cc635ee75e9d3c18df
andrew Clarke <andrewclarke.aron@gmail.com>
2023-03-01 07:38:20 +1000

    init repo

---
+++ main.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+# Python 3.11 failes to import tkinter
+
+class Windows:
+    pass
+
+
+class SignUp(Windows):
+    pass
+
+class SignIn(Windows):
+    pass
+
